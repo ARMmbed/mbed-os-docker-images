f3caf0116d0201b2a1cf1aab22239c2944bfb95d
Saheer Babu <saheer.babu@arm.com>
2021-03-26 02:02:47 +0000

    updates tmp

--- mbed-os-env/test.sh
+++ mbed-os-env/test.sh
@@ -4,3 +4,12 @@ MBED_OS_VERSION=$1
 if [ "$(uname -m)" = "x86_64" ];then
 	 exit 1
 fi
+
+            
+# set -e 
+# mbed import mbed-os-example-blinky
+# cd mbed-os-example-blinky
+# git checkout ${{ steps.build_info.outputs.MBED_OS_VERSION }}
+# mbed deploy 
+# mbed compile -m K64F -t GCC_ARM
+# echo "::set-output name=STATUS::true"
