7f0b2b0ac36ed140190816e1e2aa261213514cc4
Saheer Babu <saheer.babu@arm.com>
2021-03-26 06:34:15 +0000

    with test

--- mbed-os-env/test.sh
+++ mbed-os-env/test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -x
 MBED_OS_VERSION=$1
-# mbed import mbed-os-example-blinky
-# cd mbed-os-example-blinky
-# git checkout ${MBED_OS_VERSION}
-# mbed deploy 
-# mbed compile -m K64F -t GCC_ARM
-# mbed-tools compile -m K64F -t GCC_ARM
+mbed import mbed-os-example-blinky
+cd mbed-os-example-blinky
+git checkout ${MBED_OS_VERSION}
+mbed deploy 
+mbed compile -m K64F -t GCC_ARM
+mbed-tools compile -m K64F -t GCC_ARM
